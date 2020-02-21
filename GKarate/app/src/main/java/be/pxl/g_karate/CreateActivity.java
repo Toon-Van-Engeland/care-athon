b3a433f4e7b47272d6022b763a60d2bd28261ecd
yusufdestan <yusuf.destan@student.pxl.be>
2020-02-21 11:13:22 +0100

    create activity

--- GKarate/app/src/main/java/be/pxl/g_karate/CreateActivity.java
+++ GKarate/app/src/main/java/be/pxl/g_karate/CreateActivity.java
@@ -1,14 +1,81 @@
 package be.pxl.g_karate;
 
+import android.graphics.Color;
+import android.graphics.drawable.Icon;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateActivity  extends AppCompatActivity {
-
+    private ViewPager mViewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+        initPager();
+    }
+    private void initPager() {
+        mViewPager = findViewById(R.id.left_food_pager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new OneFragment(), "ONE");
+        adapter.addFragment(new TwoFragment(), "TWO");
+
+        mViewPager.setAdapter(adapter);
+
+    }
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mList = new ArrayList<>();
+        private final List<String> mTitleList = new ArrayList<>();
+        public ViewPagerAdapter(FragmentManager supportFragmentManager) {
+            super(supportFragmentManager);
+        }
+        @Override
+        public Fragment getItem(int i) {
+            return mList.get(i);
+        }
+        @Override
+        public int getCount() {
+            return mList.size();
+        }
+        public void addFragment(Fragment fragment, String title) {
+            mList.add(fragment);
+            mTitleList.add(title);
+        }
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTitleList.get(position);
+        }
+    }
+    public static class OneFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+            View v = inflater.inflate(R.layout.left_food_box, container, false);
+            Icon icon = Icon.createWithResource(getContext(), R.drawable.ic_left_footprint).setTint(Color.CYAN);
+            ((ImageView) v.findViewById(R.id.feet_left2)).setImageIcon(icon);
+
+
+            return v;
+        }
+    }
+    public static class TwoFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+            View v = inflater.inflate(R.layout.left_food_box, container, false);
+            Icon icon = Icon.createWithResource(getContext(), R.drawable.ic_left_footprint).setTint(Color.RED);
+            ((ImageView) v.findViewById(R.id.feet_left2)).setImageIcon(icon);
+            return v;
+        }
     }
 }
